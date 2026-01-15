e122b5004e3fe3f2d57b51125c4ae4e1331f667d
Piyush.Maurya <impiyush2003@gmail.com>
2026-01-15 21:16:52 +0530

    foodcard

--- Frontend/src/components/Body.jsx
+++ Frontend/src/components/Body.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import FoodCard from "./Foodcard";
+import FoodCard from "./FoodCard";
 
 const FoodList = () => {
   const location = useLocation();
